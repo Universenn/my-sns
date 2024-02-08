0f7349e625cb7f1677e9e48cf9c5b1612b6f709d
Universenn <dnwndhks98@gmail.com>
2024-02-08 21:37:04 +0900

    [feat] hello

--- src/main/java/com/example/mysns/controller/HelloController.java
+++ src/main/java/com/example/mysns/controller/HelloController.java
@@ -1,12 +1,14 @@
 package com.example.mysns.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v1/hello")
 public class HelloController {
-    @RequestMapping("/hello")
+    @GetMapping()
     public String hello() {
-        return "hello world";
+        return "hello";
     }
 }
