6a1d6fb4d54a71145594f1910248968a6caa0c43
Universenn <dnwndhks98@gmail.com>
2024-02-16 16:11:41 +0900

    [fix] id 변경 userid -> id

--- src/main/java/com/example/mysns/entity/User.java
+++ src/main/java/com/example/mysns/entity/User.java
@@ -19,7 +19,7 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long userId;
+    private Long id;
 
     private String email;
     private String nickname;
